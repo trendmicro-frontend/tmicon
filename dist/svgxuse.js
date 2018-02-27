63124daca48d89a03bf93756efe4a0b0ea2405ac
root <root@etslab-0.(none)>
2018-02-27 18:11:39 +0800

    Update TMIcon to v1.5

--- dist/svgxuse.js
+++ dist/svgxuse.js
@@ -79,7 +79,7 @@
         checkUseElems = function () {
             var base;
             var bcr;
-            var fallback = "https://i.icomoon.io/public/e85d3b8953/TMIcons/symbol-defs.svg?014c8d60"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
+            var fallback = "https://i.icomoon.io/public/e85d3b8953/TMIcons/symbol-defs.svg?de49aa3b"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
             var hash;
             var href;
             var i;
