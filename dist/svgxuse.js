8c01c3cb719dbb8b59fa0330309abb38b9f0df1e
Joshua Tai (HIE-TW) <joshua_tai@trend.com.tw>
2020-08-20 11:50:43 +0800

    TMIcon <v1.16>
      New icon(s): tmicon-lock-o (&#e0d8), tmicon-lock-open-o (&#e0d9)
    
      Updated icon(s): tmicon-callback (&#e501), tmicon-spyware (&#e508)

--- dist/svgxuse.js
+++ dist/svgxuse.js
@@ -79,7 +79,7 @@
         checkUseElems = function () {
             var base;
             var bcr;
-            var fallback = "https://i.icomoon.io/public/e85d3b8953/TMIcons/symbol-defs.svg?df3fad73"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
+            var fallback = "https://i.icomoon.io/public/a8317e20c1/TMIcons/symbol-defs.svg?6026f70a"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
             var hash;
             var href;
             var i;
