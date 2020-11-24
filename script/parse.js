3cacbe1cff9961be30c5ebc39931440d9ae3aed7
Joshua Tai (HIE-TW) <joshua_tai@trend.com.tw>
2020-11-24 15:16:00 +0800

    test

--- script/parse.js
+++ script/parse.js
@@ -41,99 +41,99 @@ fs.readdirSync(svgFolder).forEach(file => {
   dataFromSvgFile[file.split('.svg')[0]] = { paths, viewBox };
 });
 console.log("Incorrect viewBox count", count);
-// http.get('http://style-portal.tw.trendnet.org/api/icons/info', (res) => {
-//   const { statusCode } = res;
-//   const contentType = res.headers['content-type'];
+http.get('http://style-portal.tw.trendnet.org/api/icons/info', (res) => {
+  const { statusCode } = res;
+  const contentType = res.headers['content-type'];
 
-//   let error;
-//   // Any 2xx status code signals a successful response but
-//   // here we're only checking for 200.
-//   if (statusCode !== 200) {
-//     error = new Error('Request Failed.\n' +
-//                       `Status Code: ${statusCode}`);
-//   } else if (!/^application\/json/.test(contentType)) {
-//     error = new Error('Invalid content-type.\n' +
-//                       `Expected application/json but received ${contentType}`);
-//   }
-//   if (error) {
-//     console.error(error.message);
-//     // Consume response data to free up memory
-//     res.resume();
-//     return;
-//   }
-//   res.setEncoding('utf8');
-//   let rawData = '';
-//   res.on('data', (chunk) => { rawData += chunk; });
-//   res.on('end', () => {
-//     try {
-//       const parsedData = JSON.parse(rawData);
-//       dataParsedFromApi.iconsets = parsedData.iconsets.sort(function(a, b) {
-//         var nameA = a.id; // ignore upper and lowercase
-//         var nameB = b.id; // ignore upper and lowercase
-//         if (nameA < nameB) {
-//           return -1;
-//         }
-//         if (nameA > nameB) {
-//           return 1;
-//         }
-//         // names must be equal
-//         return 0;
-//       });
-//       dataParsedFromApi.icons = parsedData.icons.sort(function(a, b) {
-//         var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-//         var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-//         if (nameA < nameB) {
-//           return -1;
-//         }
-//         if (nameA > nameB) {
-//           return 1;
-//         }
-//         // names must be equal
-//         return 0;
-//       }).map(({ name, minorVersion, iconset, code, paths}) => {
-//         return {
-//             code,
-//             iconset,
-//             name,
-//             new: minorVersion === 17,
-//             paths
-//         }})
+  let error;
+  // Any 2xx status code signals a successful response but
+  // here we're only checking for 200.
+  if (statusCode !== 200) {
+    error = new Error('Request Failed.\n' +
+                      `Status Code: ${statusCode}`);
+  } else if (!/^application\/json/.test(contentType)) {
+    error = new Error('Invalid content-type.\n' +
+                      `Expected application/json but received ${contentType}`);
+  }
+  if (error) {
+    console.error(error.message);
+    // Consume response data to free up memory
+    res.resume();
+    return;
+  }
+  res.setEncoding('utf8');
+  let rawData = '';
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    try {
+      const parsedData = JSON.parse(rawData);
+      dataParsedFromApi.iconsets = parsedData.iconsets.sort(function(a, b) {
+        var nameA = a.id; // ignore upper and lowercase
+        var nameB = b.id; // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      });
+      dataParsedFromApi.icons = parsedData.icons.sort(function(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      }).map(({ name, minorVersion, iconset, code, paths}) => {
+        return {
+            code,
+            iconset,
+            name,
+            new: minorVersion === 17,
+            paths
+        }})
 
-//         // merge data here
-//         const combinedIcons = dataParsedFromApi.icons.map(icon => {
-//           return {
-//             ...icon,
-//             paths: dataFromSvgFile[icon.name].paths,
-//             viewBox: dataFromSvgFile[icon.name].viewBox
-//           }
-//         });
+        // merge data here
+        const combinedIcons = dataParsedFromApi.icons.map(icon => {
+          return {
+            ...icon,
+            paths: dataFromSvgFile[icon.name].paths,
+            viewBox: dataFromSvgFile[icon.name].viewBox
+          }
+        });
 
-//         // Create tmIconMap.js file for Tonic UI
-//         const TEMPLATE = `/* eslint-disable */
-// // This file is auto generated.
-// const tmIconMap = <%- JSON.stringify(ImportObj) %>;
-// export default tmIconMap;`;
+        // Create tmIconMap.js file for Tonic UI
+        const TEMPLATE = `/* eslint-disable */
+// This file is auto generated.
+const tmIconMap = <%- JSON.stringify(ImportObj) %>;
+export default tmIconMap;`;
         
-//         let output = ejs.render(TEMPLATE, {ImportObj: {
-//           ...dataParsedFromApi,
-//           icons: combinedIcons
-//         }});
-//         console.log(output);
+        let output = ejs.render(TEMPLATE, {ImportObj: {
+          ...dataParsedFromApi,
+          icons: combinedIcons
+        }});
+        console.log(output);
         
-//         fs.writeFile("tmIconMap.js", output, 'utf8', function (err) {
-//           if (err) {
-//               console.log("An error occured while writing JS Object to File.");
-//               return console.log(err);
-//           }
+        fs.writeFile("tmIconMap.js", output, 'utf8', function (err) {
+          if (err) {
+              console.log("An error occured while writing JS Object to File.");
+              return console.log(err);
+          }
         
-//           console.log("JS file has been saved.");
-//         });
+          console.log("JS file has been saved.");
+        });
 
 
-//     } catch (e) {
-//       console.error(e.message);
-//     }
-//   });
-// }).on('error', (e) => {
-//   console.error(`Got error: ${e.message}`);
-// });
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
+}).on('error', (e) => {
+  console.error(`Got error: ${e.message}`);
+});
