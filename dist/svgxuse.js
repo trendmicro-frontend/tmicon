5c494e8da35d5fec9917388e3cfb86d4df3d0302
Joshua Tai (HIE-TW) <joshua_tai@trend.com.tw>
2023-05-04 10:45:27 +0800

    TMIcon <v1.32.0>
      New icon(s): tmicon-battery (&#e102), tmicon-connecting (&#e103), tmicon-credit-token (&#e104), tmicon-feedback-dislike (&#e105), tmicon-feedback-like (&#e106), tmicon-filter-add (&#e107), tmicon-filter-delete (&#e108), tmicon-positive-benigntrue (&#e109), tmicon-positive-false (&#e10a), tmicon-positive-true (&#e10b), tmicon-scan-square (&#e10c)
    
      Updated icon(s): tmicon-warning-triangle (&#e084), tmicon-virus (&#e0b3)

--- dist/svgxuse.js
+++ dist/svgxuse.js
@@ -79,7 +79,7 @@
         checkUseElems = function () {
             var base;
             var bcr;
-            var fallback = "https://i.icomoon.io/public/a8317e20c1/TMIcons/symbol-defs.svg?310ebc1c"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
+            var fallback = "https://i.icomoon.io/public/a8317e20c1/TMIcons/symbol-defs.svg?e9fe2ac1"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
             var hash;
             var href;
             var i;
