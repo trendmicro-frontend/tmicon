4a596638bd16ca19eb5eb55ef1393c9e7d3b6a37
Joshua Tai (HIE-TW) <joshua_tai@trend.com.tw>
2023-08-31 11:04:08 +0800

    fix: add tv.js

---
+++ src/icons/tv.js
@@ -0,0 +1,13 @@
+/* AUTO-GENERATED FILE. DO NOT MODIFY. */
+const icon = {
+  "code": "e619",
+  "iconset": 1,
+  "name": "tv",
+  "new": false,
+  "paths": [
+    "M15 1h-14c-0.552 0-1 0.448-1 1v0 10c0 0.552 0.448 1 1 1v0h14c0.552 0 1-0.448 1-1v0-10c0-0.552-0.448-1-1-1v0zM14 8.414v2.586h-12v-8h12zM4 14h8v1h-8z"
+  ],
+  "viewBox": "0 0 16 16"
+};
+
+export default icon;
