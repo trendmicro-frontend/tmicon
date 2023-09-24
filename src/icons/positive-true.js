387760ee1a6f10ecd9e2259b123c7b4936688bb8
Joshua Tai (HIE-TW) <joshua_tai@trend.com.tw>
2023-09-25 00:04:23 +0800

    feat: built v1.33.0 icons

--- src/icons/positive-true.js
+++ src/icons/positive-true.js
@@ -3,7 +3,7 @@ const icon = {
   "code": "e10b",
   "iconset": 0,
   "name": "positive-true",
-  "new": true,
+  "new": false,
   "paths": [
     "M11.9 14.2l-1.5-1.4-0.4 0.3 1.9 1.9 4.1-4.9-0.4-0.3zM14 2l-8.1 9.8-3.9-3.8-2 2 6 6 10-12z"
   ],
