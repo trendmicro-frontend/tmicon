52294eb5429f6b55f640d9b214b18436c020d625
Joshua Tai (HIE-TW) <joshua_tai@trend.com.tw>
2023-10-19 14:58:56 +0800

    TMIcon <v1.38.0>
      New icon(s): tmicon-container-cluster-o (&#e121), tmicon-container-cluster (&#e124), tmicon-network-configuration (&#e119), tmicon-serverless (&#e11a), tmicon-subnet (&#e11e), tmicon-api (&#e11f), tmicon-data-storage (&#e11b), tmicon-database-o (&#e11c), tmicon-iam (&#e11d), tmicon-service (&#e117), tmicon-virtual-machine (&#e929)

--- dist/svgxuse.js
+++ dist/svgxuse.js
@@ -79,7 +79,7 @@
         checkUseElems = function () {
             var base;
             var bcr;
-            var fallback = "https://i.icomoon.io/public/a8317e20c1/TMIcons/symbol-defs.svg?5bb5a3c8"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
+            var fallback = "https://i.icomoon.io/public/a8317e20c1/TMIcons/symbol-defs.svg?e0216245cb"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
             var hash;
             var href;
             var i;
