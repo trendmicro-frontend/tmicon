5e435171a8a188a5b9fea8414ac01cf82625883a
Joshua Tai <joshua_tai@trendmicro.com>
2024-08-21 17:13:05 +0800

    fix: migrate svg builder to integrate TMIconBuddy

--- script/build-icon-svg.js
+++ script/build-icon-svg.js
@@ -1,28 +1,5 @@
 const fs = require("fs");
 const path = require("path");
-const https = require('https');
-const svgGen = require('./svgGenerator');
-
-function asyncRequest(url) {
-  return new Promise((resolve, reject) => {
-      https.get(url, (response) => {
-          let data = '';
-
-          // A chunk of data has been received.
-          response.on('data', (chunk) => {
-              data += chunk;
-          });
-
-          // The whole response has been received.
-          response.on('end', () => {
-              resolve(data);
-          });
-
-      }).on('error', (error) => {
-          reject(error);
-      });
-  });
-};
 
 (async () => {
   const preferences = require("../data/Preferences.json");
@@ -34,40 +11,56 @@ function asyncRequest(url) {
     .match(/\w\w/gi)
     .map((color) => parseInt(color, 16))
     .join(",")})`;
-
-  const cssSrc = 'https://i.icomoon.io/public/a8317e20c1/TMIcons/style-svg.css';
-  const JSRes = 'https://i.icomoon.io/public/a8317e20c1/TMIcons/svgxuse.js';
+  const cssSrc = path.join(__dirname, "..", "templates", "style-svg.css");
+  // const jsSrc = path.join(__dirname, "..", "templates", "svgxuse.js");
   const cssDist = path.join(__dirname, '..', 'dist', 'icon-svg.css');
-  const jsDist = path.join(__dirname, '..', 'dist', 'svgxuse.js');
+  // const jsDist = path.join(__dirname, '..', 'dist', 'svgxuse.js');
   const svgDest = path.join(__dirname, '..', 'dist', 'svg');
   const svgTpl = fs.readFileSync(
     path.join(__dirname, "..", "templates", "svg.tpl"),
     "utf8"
   );
+  function generateSVG(iconData, color="rgb(0, 0, 0)") {
+    const { name, grid, paths } = iconData;
+    const svgPaths = paths.map(path => `<path d="${path}" fill="${color}" />`).join('\n');
+    let svgContent = svgTpl.replace(/_viewBox/gi, iconData.grid);
+    svgContent = svgContent.replace(/_title/gi, iconData.name);
+    svgContent = svgContent.replace('<!--paths-->', svgPaths);
+    return svgContent.trim();
+  }
 
+  function generateSVGSymbols(icons) {
+    const symbols = icons.map(icon => {
+      const { name, grid, paths } = icon;
+      const svgPaths = paths.map(path => `<path d="${path}"></path>`).join('');
+      
+      return `
+    <symbol id="tm-svg-${name}" viewBox="0 0 ${grid} ${grid}">
+      ${svgPaths}
+    </symbol>`;
+    }).join('\n');
+  
+    return `
+  <svg aria-hidden="true" style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+  ${symbols}
+  </defs>
+  </svg>`.trim();
+  
+  }
   const svgDemoCSSSrc = path.join(__dirname, '..', 'css', 'icon-svg-demo.css');
   const svgDemoCSSDist = path.join(__dirname, '..', 'dist', 'demo-files', 'icon-svg-demo.css');
   const svgDemoTplPath = path.join(__dirname, '..', 'templates', 'icon-svg-demo.tpl');
   const svgDemoHTMLDest = path.join(__dirname, '..', 'dist', 'icon-svg-demo.html');
-  const svgDefsDest = path.join(__dirname, '..', 'dist', 'symbol-defs.svg');
-  const viewBox = preferences.fontPref.metrics.emSize;
   const generatedDat = new Date();
   const _generatedDate = `${generatedDat.getFullYear()}-${generatedDat.getMonth() + 1}-${generatedDat.getDate()}`;
 
   let iconGroupBySize = {};
-  let css = await asyncRequest(cssSrc);
-  let js = await asyncRequest(JSRes);
-
-  const SVGregex = /var\s+fallback\s*=\s*"([^"]+\.svg[^"]*)"/;
-  const match = SVGregex.exec(js);
-  const svgUrl = match[1];
-  const svgDefs = await asyncRequest(svgUrl);
   let svgDemoTpl = fs.readFileSync(svgDemoTplPath, 'utf8');
-
+  
+  fs.copyFileSync(cssSrc, cssDist);
+  // fs.copyFileSync(jsSrc, jsDist);
   fs.copyFileSync(svgDemoCSSSrc, svgDemoCSSDist);
-  fs.writeFileSync(svgDefsDest, svgDefs, 'utf8');
-  fs.writeFileSync(cssDist, css, 'utf8');
-  fs.writeFileSync(jsDist, js, 'utf8');
 
   const files = fs.readdirSync(svgDest);
   files.forEach(file => {
@@ -82,31 +75,14 @@ function asyncRequest(url) {
     if (a.name < b.name) return -1;
     return 0;
   });
-
   icons.forEach((icon) => {
     const targetPath = path.join(svgDest, `${icon.name.toLowerCase()}.svg`);
     icon.width = icon.height = icon.grid;
-    icon.svgPath = svgGen(icon.paths);
-    const scale = icon.height / viewBox;
-    icon.scaledPath = icon.svgPath.scale(scale);
-    icon.scaledPathData = icon.scaledPath.getPathData(true);
-
-    let svgContent = svgTpl.replace(/_viewBox/gi, icon.grid);
-    let paths = [];
-    svgContent = svgContent.replace(/_title/gi, icon.name);
-    icon.paths.forEach((path, pathIndex) => {
-      let attr = Object.assign({ fill: defaultColor }, icon.attrs[pathIndex]);
-      let genAttrs = [];
-      Object.keys(attr).forEach((key) => genAttrs.push(`${key}="${attr[key]}"`));
-      paths.push(`<path ${genAttrs.join(' ')} d="${icon.scaledPathData[pathIndex]}"></path>`);
-    });
-    svgContent = svgContent.replace('<!--paths-->', paths.join('\n  '));
-    fs.writeFileSync(targetPath, svgContent);
+    fs.writeFileSync(targetPath, generateSVG(icon, defaultColor));
     if (!iconGroupBySize[icon.grid]) iconGroupBySize[icon.grid] = [];
     iconGroupBySize[icon.grid].push(`<div class="glyph fs1"><div class="clearfix pbs"><svg class="${classSelector} ${prefix}${icon.name}"><use xlink:href="#${prefix}${icon.name}"></use></svg><span class="name"> ${prefix}${icon.name}</span></div></div>`);
   });
-
-  svgDemoTpl = svgDemoTpl.replace('_symbol', svgDefs);
+  svgDemoTpl = svgDemoTpl.replace('_symbol', generateSVGSymbols(icons));
   svgDemoTpl = svgDemoTpl.replace('_generatedDate', _generatedDate);
   svgDemoTpl = svgDemoTpl.replace('_iconSets', Object.keys(iconGroupBySize).map((size) => {
     iconGroupBySize[size].unshift(`<div class="clearfix svg-container ptl"><h1 class="grid-size mvm mtn fgc1">Grid Size: ${size} px</h1>`);
